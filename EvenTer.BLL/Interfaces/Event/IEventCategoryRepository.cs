0d39b5d7d61b85197d05dd0a444bf60c44bfff44
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-04-04 01:39:43 +0300

    add-event-interfaces

---
+++ EvenTer.BLL/Interfaces/Event/IEventCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvenTer.DAL.Entities.Events;
+
+namespace EvenTer.BLL.Interfaces.Event;
+
+public interface IEventCategoryRepository
+{
+	Task AddCategoryAsync(EventCategory category);
+	Task RemoveCategoryAsync(Guid categoryId);
+	Task UpdateCategoryAsync(Guid categoryId, EventCategory category);
+	Task<IEnumerable<EventCategory>> GetAllCategoryAsync();
+	Task<EventCategory> GetCategoryById(Guid eventid);
+	Task<EventCategory> GetCategoryByNameAsync(string categoryName);
+}
