4f70965111cb14ed820a03638c440b54a2412cd3
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-04-04 00:51:31 +0300

    add-event-dto

---
+++ EvenTer.BLL/DTO/EventDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenTer.BLL.DTO;
+
+public class EventDTO
+{
+	public string Image {  get; set; }
+	public string EventName { get; set; }
+	public string Description { get; set; }
+	public DateOnly Date { get; set; }
+	public TimeOnly StartTime { get; set; }
+	public TimeOnly? EndTime { get; set; }
+	public decimal Price { get; set; } = 0;
+	public string Location { get; set; }
+	public string? MapUrl { get; set; }
+	public int? Capacity { get; set; }
+	public Guid OrganizerId { get; set; }
+	public int CategoryId { get; set; }
+}
