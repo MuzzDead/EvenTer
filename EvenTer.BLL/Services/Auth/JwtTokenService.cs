c1f67e913485ffdeafc0db2317fec261f46dba54
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-04-07 00:27:48 +0300

    add-jwt-cfg

---
+++ EvenTer.BLL/Services/Auth/JwtTokenService.cs
@@ -0,0 +1,47 @@
+﻿using EvenTer.DAL.Entities.Users;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenTer.BLL.Services.Auth;
+
+public class JwtTokenService
+{
+	private readonly IConfiguration _configuration;
+
+	public JwtTokenService(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	public string GenerateToken(Guid userId, string username)
+	{
+		var jwtSettings = _configuration.GetSection("JwtSettings");
+
+		var claims = new[]
+		{
+			new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+			new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+			new Claim("username", username)
+		};
+
+		var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Secret"]));
+		var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+		var token = new JwtSecurityToken(
+			issuer: jwtSettings["Issuer"],
+			audience: jwtSettings["Audience"],
+			claims: claims,
+			expires: DateTime.Now.AddMinutes(double.Parse(jwtSettings["ExpiryMinutes"])),
+			signingCredentials: creds);
+
+		return new JwtSecurityTokenHandler().WriteToken(token);
+	}
+}
